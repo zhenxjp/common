6595bd3bcf9c9f92c969fde0ca90f9ac6b946d91
xjp <342023125@qq.com>
2022-08-24 15:04:02 +0800

    args add g

--- xargs.hpp
+++ xargs.hpp
@@ -36,3 +36,23 @@ static int64_t xx_get_arg_i64(int argc, char** argv, const char* key){
 #define xx_arg_have(key)  (-1 != xx_get_argidx(argc,argv,key))
 #define xx_arg_str(key)   (xx_get_arg(argc,argv,key))
 #define xx_arg_int(key)   (xx_get_arg_i64(argc,argv,key))
+
+//////////////////////////////
+struct arg_info_copy
+{
+	int argc = 0;
+	char** argv = nullptr;
+	bool init = false;
+};
+static arg_info_copy __arg_info_global;
+
+static void xxx_init_arg(int argc, char** argv)
+{
+	__arg_info_global.argc = argc;
+	__arg_info_global.argv = argv;
+	__arg_info_global.init = true;
+}
+
+#define xxx_arg_have(key)  (-1 != xx_get_argidx(__arg_info_global.argc,__arg_info_global.argv,key))
+#define xxx_arg_str(key)   (xx_get_arg(__arg_info_global.argc,__arg_info_global.argv,key))
+#define xxx_arg_int(key)   (xx_get_arg_i64(__arg_info_global.argc,__arg_info_global.argv,key))
