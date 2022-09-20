f886cdbda80538bbe82d077ccd79edb3dd96cb1d
xjp <342023125@qq.com>
2022-09-20 11:13:09 +0800

    #json #time

--- xjson_help.hpp
+++ xjson_help.hpp
@@ -9,7 +9,7 @@ using namespace nlohmann;
 static json json_from_mem(c_t mem)
 {
     json j = json::parse(mem);
-    
+
     return j;
 }
 
@@ -19,14 +19,24 @@ static json json_from_file(c_t path)
     return json_from_mem(mem.c_str());
 }
 
-static string json_to_mem(json j,int indent = 1)
+static string json_to_mem(json j, int indent = 1)
 {
     return j.dump(indent);
 }
 
-static bool json_to_file(json j,c_t path)
+static bool json_to_file(json j, c_t path)
 {
     string mem = json_to_mem(j);
-    xfile_write_new(path,mem.c_str(),mem.length());
+    xfile_write_new(path, mem.c_str(), mem.length());
     return true;
+}
+
+static json json_merge(vector<json> &all)
+{
+    json all_one;
+    for (auto o : all)
+    {
+        all_one.push_back(o);
+    }
+    return all_one;
 }
--- xtime.hpp
+++ xtime.hpp
@@ -14,3 +14,30 @@ static uint64_t get_ns()
 {
     return std::chrono::high_resolution_clock::now().time_since_epoch().count();
 }
+
+static string get_tm_date_str(tm *tt)
+{
+    stringstream ss;
+    ss<<1900+tt->tm_year<<"-"<<1+tt->tm_mon<<"-"<<tt->tm_mday;
+    return ss.str();
+}
+
+static string get_now_date_str()
+{
+    time_t now = time(nullptr);
+    tm *tt= localtime(&now);
+    return get_tm_date_str(tt);
+}
+
+
+
+static string get_tm_str(tm *tt)
+{
+    stringstream ss;
+    ss<<1900+tt->tm_year<<"-"<<1+tt->tm_mon<<"-"<<tt->tm_mday
+        <<" "
+        <<tt->tm_hour<<":"<<tt->tm_min<<":"<<tt->tm_sec;
+    return ss.str();
+}
+
+
