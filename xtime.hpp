15df20ce64dadc3d85c59aa7410c4d42407d5554
xjp <342023125@qq.com>
2022-10-12 01:06:18 +0800

    time

--- xtime.hpp
+++ xtime.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "xcom.hpp"
+#include "xstr.hpp"
 
 #define X_NS_IN_SEC (1000*1000*1000)
 static void sleep_ns(uint64_t ns){
@@ -60,4 +61,38 @@ static string get_now_str2()
     tm *tt= localtime(&now);
     return get_tm_str2(tt);
 }
+// yyyy-mm-dd
+static tm str2tm(string date)
+{
+    tm tt;
+    memset(&tt,0,sizeof(tt));
+    vector<string> temp = xsplit(date,"-");
+    tt.tm_year = stoll(temp[0]) - 1900;
+    tt.tm_mon = stoll(temp[1]) - 1;
+    tt.tm_mday = stoll(temp[2]);
+    return tt;
+}
 
+// yyyy-mm-dd
+static time_t str2time_t(string date)
+{
+    tm tt =  str2tm(date);
+    return mktime(&tt);
+}
+
+// yyyy-mm-dd
+static vector<string> get_time_in(c_t from,c_t to)
+{
+    vector<string> ret;
+    time_t from_t = str2time_t(from) + 3600 * 1;
+    time_t to_t = str2time_t(to) + 3600 * 10;
+
+    for(time_t it = from_t;it < to_t; it+= 24*3600)
+    {
+        tm *tt= localtime(&it);
+        string date_str =get_tm_date_str(tt);
+        ret.push_back(date_str);
+    }
+
+    return ret;
+}
