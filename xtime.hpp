9425ff92fc596c325316ad416d6902604209f4c7
xjp <qt00@qq.com>
2022-10-14 15:59:16 +0800

    time

--- xtime.hpp
+++ xtime.hpp
@@ -2,101 +2,175 @@
 #include "xcom.hpp"
 #include "xstr.hpp"
 
-#define X_NS_IN_SEC (1000*1000*1000)
-static void sleep_ns(uint64_t ns){
+#define X_NS_IN_SEC (1000 * 1000 * 1000)
+static void sleep_ns(uint64_t ns)
+{
     std::this_thread::sleep_for(std::chrono::nanoseconds(ns));
 }
 
-static void sleep_ms(uint64_t ms){
-    sleep_ns(ms*1000*1000);
+static void sleep_ms(uint64_t ms)
+{
+    sleep_ns(ms * 1000 * 1000);
 }
 
 static uint64_t get_ns()
 {
     return std::chrono::high_resolution_clock::now().time_since_epoch().count();
 }
+//////////////////////////////////////////////////////////////////////////////////////
 
-// get "yyyy-mm-dd" by tm
-static string get_tm_date_str(tm *tt)
+
+
+#define XX_TIME_FORMAT "%02d-%02d-%02d"
+#define XX_DATE_FORMAT "%04d-%02d-%02d"
+#define XX_DATE_TIME_FORMAT "%04d-%02d-%02d %02d-%02d-%02d"
+#define XX_DATE_TIME_FORMAT2 "%04d-%02d-%02d_%02d_%02d_%02d"
+class xx_time
 {
-    char temp[100]  = {0};
-    sprintf(temp,"%d-%02d-%02d",
-        1900+tt->tm_year,
-        1+tt->tm_mon,
-        tt->tm_mday);
-
-    return temp;
-}
-
-// get "yyyy-mm-dd" by now
-static string get_now_date_str(int diff_day = 0)
-{
-    time_t now = time(nullptr);
-    now -= diff_day*24*3600;
-    tm *tt= localtime(&now);
-    return get_tm_date_str(tt);
-}
-
-
-// get "yyyy-mm-dd hh:mm:ss" bt tm
-static string get_tm_str(tm *tt)
-{
-    stringstream ss;
-    ss<<1900+tt->tm_year<<"-"<<1+tt->tm_mon<<"-"<<tt->tm_mday
-        <<" "
-        <<tt->tm_hour<<":"<<tt->tm_min<<":"<<tt->tm_sec;
-    return ss.str();
-}
-
-// get "yyyy-mm-dd__hh_mm_ss" by tm
-static string get_tm_str2(tm *tt)
-{
-    stringstream ss;
-    ss<<1900+tt->tm_year<<"-"<<1+tt->tm_mon<<"-"<<tt->tm_mday
-        <<"__"
-        <<tt->tm_hour<<"_"<<tt->tm_min<<"_"<<tt->tm_sec;
-    return ss.str();
-}
-
-// get "yyyy-mm-dd__hh_mm_ss" by now
-static string get_now_str2()
-{
-    time_t now = time(nullptr);
-    tm *tt= localtime(&now);
-    return get_tm_str2(tt);
-}
-// yyyy-mm-dd
-static tm str2tm(string date)
-{
-    tm tt;
-    memset(&tt,0,sizeof(tt));
-    vector<string> temp = xsplit(date,"-");
-    tt.tm_year = stoll(temp[0]) - 1900;
-    tt.tm_mon = stoll(temp[1]) - 1;
-    tt.tm_mday = stoll(temp[2]);
-    return tt;
-}
-
-// yyyy-mm-dd
-static time_t str2time_t(string date)
-{
-    tm tt =  str2tm(date);
-    return mktime(&tt);
-}
-
-// yyyy-mm-dd
-static vector<string> get_time_in(c_t from,c_t to)
-{
-    vector<string> ret;
-    time_t from_t = str2time_t(from) + 3600 * 1;
-    time_t to_t = str2time_t(to) + 3600 * 10;
-
-    for(time_t it = from_t;it < to_t; it+= 24*3600)
+public:
+    xx_time()
     {
-        tm *tt= localtime(&it);
-        string date_str =get_tm_date_str(tt);
-        ret.push_back(date_str);
+        clean();
+    }
+    xx_time(const string& str,c_t format = XX_DATE_TIME_FORMAT)
+    {
+        init(str.c_str(),format);
+    }
+    xx_time(time_t t)
+    {
+        init(t);
+    }
+    xx_time(time_t t,int diff)
+    {
+        init(t);
+        day_diff(diff);
+    }
+
+    void day_diff(int n)
+    {
+        time_t t = to_time();
+        t += n*24*3600;
+        init(t);
+    }
+
+    // "yyyy-mm-dd hh:mm:ss"
+    void init(c_t str,c_t format = XX_DATE_TIME_FORMAT)
+    {
+        clean();
+        sscanf(str, format,
+               &x_.tm_year, &x_.tm_mon, &x_.tm_mday,
+               &x_.tm_hour, &x_.tm_min, &x_.tm_sec);
+        to_sys_tm();
+    }
+
+    void init(time_t t)
+    {
+        clean();
+        if(0 == t)
+            t = time(nullptr);
+
+        tm *tt = localtime(&t);
+        x_ = *tt;
+    }
+
+    // yyyy-mm-dd
+    string to_date_str(const char* format = XX_DATE_FORMAT)
+    {
+        to_user_tm();
+        char temp[100] = {0};
+        sprintf(temp, format,
+                x_.tm_year,x_.tm_mon,x_.tm_mday);
+        to_sys_tm();
+        return temp;
+    }
+    // hh:mm:ss
+    string to_time_str(const char* format = XX_TIME_FORMAT)
+    {
+        to_user_tm();
+        char temp[100] = {0};
+        sprintf(temp, format,
+                x_.tm_hour,x_.tm_min,x_.tm_sec);
+        to_sys_tm();
+        return temp;
+    }
+
+    // yyyy-mm-dd hh:mm:ss
+    string to_date_time_str(const char* format = XX_DATE_TIME_FORMAT)
+    {
+        to_user_tm();
+        char temp[100] = {0};
+        sprintf(temp, format,
+                x_.tm_year,x_.tm_mon,x_.tm_mday,
+                x_.tm_hour,x_.tm_min,x_.tm_sec);
+        to_sys_tm();
+        return temp;
+    }
+
+    time_t to_time(){
+        return mktime(&x_);
+    }
+    
+private:
+    void clean()
+    {
+        memset(&x_, 0, sizeof(x_));
+    }
+    void to_sys_tm()
+    {
+        x_.tm_year -= 1900;
+        x_.tm_mon -= 1;
+    }
+
+    void to_user_tm()
+    {
+        x_.tm_year += 1900;
+        x_.tm_mon += 1;
+    }
+private:
+    tm x_;
+};
+
+
+// yyyy-mm-dd
+// []
+static vector<string> get_time_in(c_t from, c_t to)
+{
+    xx_time xx_from;
+    xx_time xx_to;
+
+    xx_from.init(from);
+    xx_to.init(to);
+
+    vector<string> ret;
+    while(true)
+    {
+        if(xx_from.to_time() > xx_to.to_time())
+            break;
+        ret.push_back(xx_from.to_date_str());
+        xx_from.day_diff(1);
     }
 
     return ret;
+}
+
+
+static void time_test()
+{
+    const char* xxxx= "2020-05-08";
+    xx_time xx;
+    xx.init(xxxx);
+    cout<<xx.to_date_str();
+    cout<<endl;
+    cout<<xx.to_date_time_str();
+    cout<<endl;
+    cout<<xx.to_time_str();
+    cout<<endl;
+
+    xx.day_diff(1);
+    cout<<xx.to_date_time_str();
+    cout<<endl;
+
+    xx.day_diff(30);
+    cout<<xx.to_date_time_str();
+    cout<<endl;
 }
