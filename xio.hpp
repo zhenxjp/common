c8253c6272e8032ce09fd2e37906f0a1907f107f
xjp <qt00@qq.com>
2025-01-24 14:47:51 +0800

    io

--- xio.hpp
+++ xio.hpp
@@ -2,6 +2,8 @@
 #include "xcom.hpp"
 #include "xio_idx.hpp"
 #include "xfile.hpp"
+#include <map>
+#include <mutex>
 
 enum class io_rw_type : char {
     rw_read,
@@ -26,6 +28,49 @@ struct io_context
 };
 
 
+class io_idx_mgr
+{
+public:
+    io_idx* get_idx(const string &key)
+    {
+        std::lock_guard<std::mutex> lock(lock_);
+        auto it = idx_map_.find(key);
+        if(it != idx_map_.end())
+        {
+            return it->second;
+        }else{
+            io_idx *idx = new io_idx();
+            idx_map_.insert(std::make_pair(key,idx));
+            return idx;
+        }
+    }
+    static io_idx_mgr& get_instance()
+    {
+        return instance_;
+    }
+private:
+    std::mutex lock_;
+    std::map<string,io_idx*>    idx_map_;
+    static io_idx_mgr instance_;
+};
+
+
+static void calc_iov_cnt(iovec *iov,uint32_t iov_cnt,uint32_t tot_len,
+                            uint32_t &iov_full_cnt,uint32_t &remain)
+{
+    iov_full_cnt = 0;
+    remain = tot_len;
+    for(int i = 0; i < iov_cnt; i++)
+    {
+        if(remain >= iov[i].iov_len)
+        {
+            iov_full_cnt++;
+            remain -= iov[i].iov_len;
+        }else{
+            break;
+        }
+    }
+}
 
 // ./io_save/io_save_20170512.index
 // ./io_save/io_save_20170512.0
@@ -33,46 +78,135 @@ struct io_context
 class io
 {
 public:
+    ~io()
+    {
+        release();
+    }
     int init(const io_context &ctx)
     {
         ctx_ = ctx;
         xfile_fix_foder(ctx_.path_);
         idx_path_ = ctx_.path_ + ctx_.prefix_ + ".index";
         data_pre_ = ctx_.path_ + ctx_.prefix_ + ".";
+        idx_ = io_idx_mgr::get_instance().get_idx(data_pre_);
 
         if (io_rw_type::rw_write == ctx_.rw_type_)
         {
             if(io_init_type::init_new ==ctx_.init_type_)
             {
-                return write_new();
+                return init_write_new();
             }else
             {
-                return write_exist();
+                return init_write_exist();
             }
         }else{
-
+            
         }
         
         return 0;
     }
-
-    int write_data(iovec *iov,uint64_t cnt,uint32_t &written)
+    // 不许跨文件读
+    int read_data(iovec *iov,uint32_t cnt,uint32_t start_idx,uint32_t &read)
     {
-        // todo check blk
+        read = 0;
         
+        uint32_t blk_max = idx_->meta_.blk_size_;
 
+        uint32_t blk_no = idx_->cnt_ % blk_max;
+        uint32_t file_no = idx_->cnt_ / blk_max;
+        uint32_t offset = idx_->get_blk_start(file_no,blk_no);
 
-        
-    }
-private:
-    int write_exist()
-    {
-        idx_.load_exist(ctx_.meta_,idx_path_);
+        cnt = std::min(blk_max - blk_no,cnt);// 不跨文件
+        cnt = std::min(idx_->cnt_ - start_idx,(uint64_t)cnt);// 不能超过最多大数量
+        if(0 == cnt)
+            return 0;
+
+        idx_->fill_iov_len(iov,cnt,file_no,blk_no);
+        ssize_t rret = preadv(fds_[file_no],iov,cnt,offset);
+        uint32_t remain = 0;
+        calc_iov_cnt(iov,cnt,rret,read,remain);
+        if (0 != remain || cnt != read)
+            printf("xxx read war rret:%ld,read:%u,remain:%u\n",rret,read,remain);
         return 0;
     }
 
 
-    int write_new()
+
+    // 不许跨文件写
+    int write_data(iovec *iov,uint32_t cnt,uint32_t &written)
+    {
+        written = 0;
+
+        uint32_t blk_max = idx_->meta_.blk_size_;
+
+        uint32_t blk_no = idx_->cnt_ % blk_max;
+        uint32_t file_no = idx_->cnt_ / blk_max;
+        
+        cnt = std::min(blk_max - blk_no,cnt);
+        uint32_t offset = idx_->get_blk_start(file_no,blk_no);
+
+        // 需要新文件
+        if(-1 == fds_[file_no])
+        {
+            assert(0 == blk_no);
+            create_file(file_no);
+        }
+
+        // 写一次，能写多少是多少
+        ssize_t wret = pwritev(fds_[file_no],iov,cnt,offset);
+
+        uint32_t remain = 0;
+        calc_iov_cnt(iov,cnt,wret,written,remain);
+        if (0 != remain || cnt != written)
+            printf("xxx write war wret:%ld,written:%u,remain:%u\n",wret,written,remain);
+
+        idx_->add_idx(iov,written,file_no,blk_no);
+
+
+        return 0;
+    }
+
+    void release()
+    {
+        for (size_t i = 0; i < max_file_cnt_; i++)
+        {
+            if (-1 != fds_[i])
+            {
+                close(fds_[i]);
+                fds_[i] = -1;
+            }
+        }
+        delete [] fds_;
+    }
+
+private:
+    void init_fd_array()
+    {
+        int64_t max_file_cnt_ = 65535;
+        int64_t sys_max_open_cnt = sysconf(_SC_OPEN_MAX);
+        if(-1 != sys_max_open_cnt && sys_max_open_cnt < max_file_cnt_)
+            max_file_cnt_ = sys_max_open_cnt;
+        fds_ = new int[max_file_cnt_];
+        std::fill_n(fds_,max_file_cnt_,-1);
+    }
+
+    int create_file(uint32_t file_no)
+    {
+        string path = data_pre_ + std::to_string(file_no);
+        int fd = open(path.c_str(),O_RDWR|O_CREAT|O_TRUNC,0666);
+        fds_[file_no] = fd;
+
+        idx_->create_idx_to_vec();
+        return 0;
+    }
+
+    int init_write_exist()
+    {
+        idx_->load_exist(ctx_.meta_,idx_path_);
+        return 0;
+    }
+
+    int init_write_new()
     {
         // 创建目录
         bool c_ret = xfile_create_foder(ctx_.path_);
@@ -83,17 +217,17 @@ private:
         printf("del cmd:%s\n",cmd.c_str());
         system(cmd.c_str());
         
-        idx_.create_new(ctx_.meta_,idx_path_);
+        idx_->create_new(ctx_.meta_,idx_path_);
         return 0;
     }
 public:
-    uint32_t w_f_no_ = 0;
-    uint32_t w_f_idx_ = 0;
-    vector<int> fds_;
+
+    int *fds_ = nullptr;
+    uint32_t max_file_cnt_ = 0;
 
     io_context ctx_;
     
-    io_idx idx_;
+    io_idx *idx_;
     string idx_path_ = "";
 
     string data_pre_ = "";
--- xio_idx.hpp
+++ xio_idx.hpp
@@ -77,12 +77,9 @@ public:
         
         while (idx_cnt)
         {
-            idx_t *offset_array = new idx_t[meta_.blk_cnt_max_];
-            memset(offset_array,0,IDX_LEN * meta_.blk_cnt_max_);
-            index_.push_back(offset_array);
-
+            idx_t *offset_array = create_idx_to_vec();
             uint64_t read_cnt = std::min(meta_.blk_cnt_max_,idx_cnt);
-
+            
             ssize_t read_len = xread(fd_,offset_array,read_cnt * IDX_LEN);
 
             if(read_cnt * IDX_LEN != read_len)
@@ -93,9 +90,10 @@ public:
             idx_cnt -= read_cnt;
         }
         cnt_ = idx_cnt;
-
+        
         return 0;
     }
+
 
     int create_new(const io_meta& meta,const string &path)
     {
@@ -103,7 +101,7 @@ public:
         fd_ = open(path.c_str(),O_RDWR|O_CREAT|O_TRUNC,0666);
         CHECK_RETV(fd_,-1);
 
-        write(fd_,&meta_,META_LEN);
+        xwrite(fd_,&meta_,META_LEN);
         return 0;
     }
 
@@ -117,6 +115,57 @@ public:
         close(fd_);
     }
 
+    uint32_t get_blk_end(uint32_t file_no,uint32_t blk_no)
+    {
+        assert(file_no < index_.size());
+        assert(blk_no  < meta_.blk_cnt_max_);
+        return index_[file_no][blk_no];
+    
+    }
+    uint32_t get_blk_start(uint32_t file_no,uint32_t blk_no)
+    {
+        assert(file_no < index_.size());
+        assert(blk_no  < meta_.blk_cnt_max_);
+        if(0 == blk_no)
+            return 0;
+        return index_[file_no][blk_no-1];
+    }
+
+    
+
+    int add_idx(iovec *iov,uint32_t cnt,uint32_t file_no,uint32_t blk_no)
+    {
+        assert(file_no < index_.size());
+        assert(blk_no + cnt <= meta_.blk_cnt_max_);
+        assert(-1 != fd_);
+        for (size_t i = 0; i < cnt; i++)
+        {
+            index_[file_no][blk_no+i] = iov[i].iov_len;
+        }
+        xwrite(fd_,index_[file_no]+blk_no,cnt*IDX_LEN);
+        cnt_+=cnt;
+        return 0;
+    }
+
+    int fill_iov_len(iovec *iov,uint32_t cnt,uint32_t file_no,uint32_t blk_no)
+    {
+        assert(file_no < index_.size());
+        assert(blk_no + cnt <= meta_.blk_cnt_max_);
+
+        for(size_t i = 0; i < cnt; i++)
+        {
+            iov[i].iov_len = get_blk_end(file_no,blk_no+i) - get_blk_start(file_no,blk_no+i);
+        }
+        
+        return 0;
+    }
+
+    idx_t* create_idx_to_vec()
+    {
+        idx_t *offset_array = new idx_t[meta_.blk_cnt_max_];
+        index_.push_back(offset_array);
+        return offset_array;
+    }
 public:
     uint64_t cnt_ = 0;                      // 已写索引数量
 
